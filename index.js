bbc8586cf32c696d1c90261f8a9de9a1ecc4ed94
Prakash Anand <ppa58514@gmail.com>
2020-09-22 14:48:25 +0530

    Done

--- index.js
+++ index.js
@@ -4,21 +4,18 @@ const bodyParser = require("body-parser");
 const port = 3000
 const serialiser = require('node-serialize')
 const mongodb = require('mongodb');
-
-app.use(express.urlencoded());
-
-
-// Parse JSON bodies (as sent by API clients)
-const mongoURI = "mongodb://localhost:27017"+ "/blog"
-console.log(mongoURI);
-
-app.use(express.json());
-const connection = mongodb.MongoClient.connect(mongoURI,(err, dbClient) =>{
-    if(err){
-        console.log('connection failed')
-        return
-    }
+const mongoose = require('mongoose')
+const blogsRoute = require('./routes/blog');
+const cors = require('cors')
+app.use(bodyParser.json())
+app.use('/blog',blogsRoute);
+app.use(cors())
+// DB CONNECTION
+mongoose.connect("mongodb+srv://root:toor@pizza.uja22.mongodb.net/Blog?retryWrites=true&w=majority",{ useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology:true,useFindAndModify:true
+},()=>{
+    console.log("Connected to db");
 })
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
